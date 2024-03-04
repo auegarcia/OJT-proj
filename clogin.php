a9558de100b2c95d67dd5478e52fdcf6041c422a
tscunanan <tscunanan@mymail.mapua.edu.ph>
2024-03-05 05:37:58 +0800

    iso changes

--- clogin.php
+++ clogin.php
@@ -1,18 +1,15 @@
 <?php
 session_start();
+include ('dbcon.php');
+
 $username = ($_POST['username']);
 $password = ($_POST['password']);
-$db_name = "users";
-$db_username = "root";
-$db_pass = "";
-$db_host = "localhost";
-$con = mysqli_connect("$db_host","$db_username","$db_pass", "$db_name") 
-or die(mysqli_error()); 
+
 $query = "SELECT * from users WHERE username='$username'";
 $results = mysqli_query($con, $query); 
 $exists = mysqli_num_rows($results); 
 
-$table_usersname = "";
+$table_username = "";
 $table_password = "";
 if($results != "") 
 {
---
+++ dbcon.php
@@ -0,0 +1,18 @@
+<?php
+if (!isset($_SESSION)) {
+  // no session has been started yet
+  session_start();
+}
+//$conn = mysqli_connect("us-cdbr-east-06.cleardb.net",'b2f4b986604b45','0290b9ac','heroku_fadbb188c837616');
+
+//NEW
+$con = mysqli_connect("localhost",'root','','users');
+
+
+//LOCAL
+//$conn = mysqli_connect("localhost","root","","caremate");
+
+if (!$conn) {
+    die("Connection Failed :-".mysqli_connect_error());
+}
+?>
